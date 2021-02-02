b84d618fbded5a5b698ab265c06e4b7e542f038c
Kelli Strawbridge <69655334+Austinkst@users.noreply.github.com>
2021-02-01 21:57:12 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,2 +1,28 @@
-const express = require("express");
+var express = require("express");
 
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(express.static("public"));
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+
+var routes = require("./controllers/burgers_Controller.js");
+
+app.use(routes);
+
+
+app.list(PORT, function() {
+  
+  console.log("Server listening on: http://localhost:" + PORT);
+});
