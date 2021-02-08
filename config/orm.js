637a84898c332e016a4eaea3764b901a0b6ea292
Kelli Strawbridge <69655334+Austinkst@users.noreply.github.com>
2021-02-07 23:55:55 -0500

    Update orm.js

--- config/orm.js
+++ config/orm.js
@@ -35,9 +35,7 @@ var orm = {
     all: function (tableInput, cb) {
       var queryString = "SELECT * FROM " + tableInput + ";";
       connection.query(queryString, function(err, result) {
-        if (err) {
-        throw err;
-        }  
+        if (err) throw err;
         cb(result);
     });
 },
