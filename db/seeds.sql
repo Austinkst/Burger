34fe3287b06f750ea4304902e2796d3be89d696d
Kelli Strawbridge <69655334+Austinkst@users.noreply.github.com>
2021-02-08 00:02:40 -0500

    Update seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, destroyed)
+VALUES ("Black n Blue", true);
+
+INSERT INTO burgers (burger_name, destroyed)
+VALUES ("Uncle Sam", true);
+
+INSERT INTO burgers (burger_name, destroyed)
+VALUES ("Quarter Pounder w/Cheese", true);
