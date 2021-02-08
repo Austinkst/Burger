8e22b5009db928eea09c3ecfb7465c1fe1785040
Kelli Strawbridge <69655334+Austinkst@users.noreply.github.com>
2021-02-07 23:53:00 -0500

    bug fixes

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 var mysql = require("mysql");
 
+let connection;
 
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "",
     database: "burger_db"
-});
+ });
+};
 
 connection.connect(function(err) {
     if (err) {
